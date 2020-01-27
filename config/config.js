3878add64b2ecdf6db63844ca88ab0b33b87a219
Muhammad Abdurrozaq <abduroza@students.unnes.ac.id>
2020-01-27 09:31:45 +0700

    edit config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
+const dbProdUsername = process.env.DBPROD_USERNAME;
+const dbProdPassword = process.env.DBPROD_PASSWORD;
+const dbProdName = process.env.DBPROD_NAME;
+const dbProdHost = process.env.DBPROD_HOST;
 
 module.exports = {
   development: {
@@ -21,10 +25,10 @@ module.exports = {
     operatorsAliases: false
   },
   production: {
-    username: dbUsername,
-    password: dbPassword,
-    database: dbName,
-    host: "127.0.0.1",
+    username: dbProdUsername,
+    password: dbProdPassword,
+    database: dbProdName,
+    host: dbProdHost,
     dialect: "postgres",
     operatorsAliases: false
   }
